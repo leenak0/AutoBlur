2a897336922b8b199c52cf2edd716b60f796d996
나경 <39471985+leenak0@users.noreply.github.com>
2019-10-30 23:15:36 +0900

    Update SelectPic.java

--- app/src/main/java/com/leenak0/project/autoblur/SelectPic.java
+++ app/src/main/java/com/leenak0/project/autoblur/SelectPic.java
@@ -37,8 +37,8 @@ public class SelectPic extends AppCompatActivity {
     File uploadFile;
     String imageFileName;
     Uri photoUri;
-    String firebaseurl="gs://autoblur123.appspot.com/";
-    String serverurl="http://52.78.40.106/recogination/";
+    String firebaseurl=**********;
+    String serverurl=**********;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
